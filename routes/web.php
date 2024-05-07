eeadf033625fad55a4a08e1abc18b2c00069438e
Muhammad Syairozy Syauqi <syairozys@gmail.com>
2024-05-07 14:20:49 +0700

    HTTP Exception

--- routes/web.php
+++ routes/web.php
@@ -165,3 +165,14 @@ Route::get('/error/validation', function (){
     throw new ValidationException("Validation Error");
 });
 
+Route::get('/abort/400', function (){
+    abort(400, "Ups Validation Error");
+});
+
+Route::get('/abort/401', function (){
+    abort(401);
+});
+
+Route::get('/abort/500', function (){
+    abort(500);
+});
